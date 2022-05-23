b81aab5bad4c5fe7f1764986d8474cbcf9e4c6a3
Moe Tahan <mohamadtahan880@gmail.com>
2022-05-24 01:35:36 +0300

    JS: Hover over S to start game

---
+++ game.js
@@ -0,0 +1,32 @@
+window.onload = function () {
+    var start = document.getElementById("start");
+    var end = document.getElementById("end");
+    var game = document.getElementById("game");
+    var boundary = document.querySelectorAll(".boundary");
+    var score = 0;
+  
+  
+  
+    start.addEventListener("mouseover", function () {
+      liveTime();
+      for (var i = 0; i < boundary.length; i++) {
+        
+        document.getElementById("status").innerHTML = "Game Has Started :)";
+        boundary[i].addEventListener("mouseover", function () {
+          for (var i = 0; i < boundary.length; i++) {
+            boundary[i].classList.add("youlose");
+            document.getElementById("status").innerHTML =
+              "You Lost. Press S to reload.";
+              myStopFunction()
+          }
+  
+          score -= 10;
+          document.querySelector(".example").innerHTML = "score= " + score;
+        });
+      }
+    });
+  
+    mouseOut();
+    endGame();
+    restart();
+  };
